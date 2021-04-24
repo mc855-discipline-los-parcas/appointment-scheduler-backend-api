4718c263a8e71a8ab45a77a8980916daaa764839
gabrieloswaldo <gabriel.oswaldo99@gmail.com>
2021-04-23 23:15:38 -0300

    Create base project structure

---
+++ src/main/java/br/unicamp/cecom/appointmentscheduler/AppointmentSchedulerBackendApiApplication.java
@@ -0,0 +1,13 @@
+package br.unicamp.cecom.appointmentscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentSchedulerBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentSchedulerBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unicamp/cecom/appointmentscheduler/AppointmentSchedulerBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unicamp.cecom.appointmentscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentSchedulerBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
